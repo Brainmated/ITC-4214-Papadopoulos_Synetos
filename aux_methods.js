16d37f548274eab351a3168833b3e3e0bbf05779
Orestis Papadopoulos <o.papadopoulos@acg.edu>
2022-10-11 20:31:48 +0300

    Poor navigation bar.

---
+++ aux_methods.js
@@ -0,0 +1,14 @@
+// all this is supposed to be for the sticky header
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
