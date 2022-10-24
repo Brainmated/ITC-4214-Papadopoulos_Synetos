ca3ab12d876fbd0a9025cf85a08fe10aaa7fca6d
Brainmated <93430275+Brainmated@users.noreply.github.com>
2022-10-24 11:38:08 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    
+    function scrollFunction(){
+        if(window.pageYOffset > sticky){
+            Headers.classList.add("sticky");
+        }
+        else{
+            Headers.classList.remove("sticky");
+        }
+    }
+    window.onscroll= function(){
+        scrollFunction()
+    };
+
+    $("#register").click(function(){
+
+    
+    /*
+    $("#register").on("click",function(){
+      
+      var title=$("#bookTitle").val();
+      var author=$("#author").val();
+      
+      var data=<tr><td>+title+</td>+
+                      <td>+author</td></tr>;
+      $("#myTable").append(data);
+      
+    });*/
+
+        $("#tbody").append("<tr></tr>");
+        $("tr").append("<td></td>");
+
+        var l = $("bookTitle").length;
+
+        $("#myTable") .empty();
+        for(var i = 0; i<l; i+=1){
+            $("td").append($("#bookTitle").val());
+        } 
+
+        $("#tbody").append("<tr></tr>");
+        $("tr").append("<td></td>");
+
+        var la = $("author").length;
+
+        $("#myTable") .empty();
+        for (var x = 0; x<la; x+=1){
+            $("td").append($("#author").val());
+        }
+        
+        /*$("#tbody").append("<tr></tr>");
+        $("tr").append("<td></td>");
+        $("td").append($("#bookTitle").val());*/
+    });
+    $("#myTable").on("click", "#remove", function(){
+        var inRow = this.parentNode.parentNode.rowIndex;
+        document.getElementById("myTable").deleteRow(inRow);
+    });
+    /*
+    function removal(r){
+        $(r).parents.("tr").remove();
+    }
+    */
+});
