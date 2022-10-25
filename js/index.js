20e8fc76236851215368e227872b065186d197eb
Brainmated <93430275+Brainmated@users.noreply.github.com>
2022-10-25 15:10:13 +0300

    Updated function

--- js/index.js
+++ js/index.js
@@ -1,63 +1,31 @@
-$(document).ready(function(){
-    
-    function scrollFunction(){
-        if(window.pageYOffset > sticky){
-            Headers.classList.add("sticky");
+$document.ready(function(){
+
+    var blankRow = "<tr><td>Nothing to display</td></tr>";
+    $("#myTable tbody").append(blankRow); //will add an ampty row every time the page reloads
+    $("#regbtn").click(function(){
+        var btitle = $("#bookTitle").val().trim();
+        var byear = $("#bookYear").val().trim();
+
+        if(btitle!="" && byear!=""){
+            if($("#myTable tbody").children().childrent().length==1){
+                $("#myTable tbody").html("");
+            }
+            var addRow = "<tr><td>"+btitle+"</td><td>"+byear+"</td><td><button class='rembtn'>Delete Row</button></td></tr>";
+
+            $("#myTable tbody").append(addRow);
+            $("#btitle").val("");
+            $("#byear").val("");
+
+            $("rembtn").click(function(){
+                $(this).parent().parent().remove();
+
+                if($("#myTable tbody").children().childrent().length==0){
+                    $("#myTable tbody").append(blankRow);
+                }
+            });
         }
         else{
-            Headers.classList.remove("sticky");
+            alert("Error: Please provide inpute for all fields")
         }
-    }
-    window.onscroll= function(){
-        scrollFunction()
-    };
-
-    $("#register").click(function(){
-
-    
-    /*
-    $("#register").on("click",function(){
-      
-      var title=$("#bookTitle").val();
-      var author=$("#author").val();
-      
-      var data=<tr><td>+title+</td>+
-                      <td>+author</td></tr>;
-      $("#myTable").append(data);
-      
-    });*/
-
-        $("#tbody").append("<tr></tr>");
-        $("tr").append("<td></td>");
-
-        var l = $("bookTitle").length;
-
-        $("#myTable") .empty();
-        for(var i = 0; i<l; i+=1){
-            $("td").append($("#bookTitle").val());
-        } 
-
-        $("#tbody").append("<tr></tr>");
-        $("tr").append("<td></td>");
-
-        var la = $("author").length;
-
-        $("#myTable") .empty();
-        for (var x = 0; x<la; x+=1){
-            $("td").append($("#author").val());
-        }
-        
-        /*$("#tbody").append("<tr></tr>");
-        $("tr").append("<td></td>");
-        $("td").append($("#bookTitle").val());*/
     });
-    $("#myTable").on("click", "#remove", function(){
-        var inRow = this.parentNode.parentNode.rowIndex;
-        document.getElementById("myTable").deleteRow(inRow);
-    });
-    /*
-    function removal(r){
-        $(r).parents.("tr").remove();
-    }
-    */
 });
