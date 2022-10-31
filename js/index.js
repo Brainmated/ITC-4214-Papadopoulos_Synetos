8ee36a18a264dc45cac089fc876bfab91f62c0bb
Brainmated <93430275+Brainmated@users.noreply.github.com>
2022-10-31 12:42:08 +0200

    Updated tablesorting.js and index.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,49 @@
-$(document).ready(function(){
-    
-    var blankRow = "<tr><td>Nothing to display</td></tr>";
-    $("#myTable tbody").append(blankRow); //will add an ampty row every time the page reloads
-    $("#btn btn-primary mb-2").click(function(){
+$(document).ready(function() {
 
-        var rownum=1;
-        var btitle = $("#txt_field_book_title").val().trim();
-        var byear = $("#txt_field_year_of_publication").val().trim();
+  // orestis start
+  const current_year = new Date().getFullYear();
 
-        if(btitle!="" && byear!=""){
-            if($("#myTable tbody").children().children().length==1){
-                $("#myTable tbody").html("");
-            }
-            var addRow = "<tr><td>"+btitle+"</td><td>"+byear+"</td><td><button class='rembtn' onclick='remove(this)'>Delete Row</button></td></tr>";
+  // set the max year of publication to be the current date
+  $("#txt_field_year_of_publication").attr({
+      max: get_current_year
+  });
 
-            $("#myTable tbody").append(addRow);
-            $("#rownum").append(rownum+1);
-            $("#btitle").val("");
-            $("#byear").val("");
+  function get_current_year() {
+      return current_year;
+  }
 
-            //rembtn function to remove the row and its contents
-            
-        }
-        else{
-            alert("Error: Please provide input for all fields")
-        }
-    });
+  // orestis end
+  
+  var blankRow = "<tr><td></td></tr>";
+  $("#myTable tbody").append(blankRow); //will add an ampty row every time the page reloads
+  $("#add").click(function(){
+      var btitle = $("#txt_field_book_title").val().trim();
+      var byear = $("#txt_field_year_of_publication").val().trim();
+
+      if(btitle!="" && byear!=""){
+          if($("#myTable tbody").children().children().length==1){
+              $("#myTable tbody").html("");
+          }
+          var addRow = "<tr><td>"+btitle+"</td><td>"+byear+"</td><td><button class='rembtn' onclick='remove(this)'>Delete Row</button></td></tr>";
+
+          $("#myTable tbody").append(addRow);
+          $("#btitle").val("");
+          $("#byear").val("");
+
+          //rembtn function to remove the row and its contents
+          
+      }
+      else{
+          alert("Error: Please provide input for all fields")
+      }
+  });
 });
-function remove(elem){
-    $(elem).parent().parent().remove();
 
-    if($("#myTable tbody").children().children().length==0){
-        var blankRow = "<tr><td>Nothing to display</td></tr>";
-        $("#myTable tbody").append(blankRow);
-    }
+function remove(elem){
+  $(elem).parent().parent().remove();
+
+  if($("#myTable tbody").children().children().length==0){
+      var blankRow = "<tr><td>Nothing to display</td></tr>";
+      $("#myTable tbody").append(blankRow);
+  }
 }
--- js/tablesorting.js
+++ js/tablesorting.js
@@ -1,47 +1,48 @@
-$(document).ready(function(){
-
-    var sortOrder = 1; // flag to toggle the sorting order
-    function getVal(elm, colIndex){
-        var td = $(elm).children('td').eq(colIndex).text();
-        if(typeof td !== "undefined"){
-            var v = td.toUpperCase();
-            if($.isNumeric(v)){
-                v = parseInt(v,10);
-            }
-            return v;
-        }
-    }
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
     
-    $(document).on('click', '.sortable', function(){
-        var self = $(this);
-        var colIndex = self.prevAll().length;
-        var o = (sortOrder == 1) ? 'asc' : 'desc'; // you can use for css to show sort direction
-        sortOrder *= -1; // toggle the sorting order
+    dir = "asc";
+    
+    while (switching) {
+    
+      switching = false;
+      rows = table.rows;
+      
+      for (i = 1; i < (rows.length - 1); i++) {
         
-        $('.sortable').removeClass('asc').removeClass('desc');
-        self.addClass(o);
-    
-        var tbody = self.closest("myTable").find("tbody");
-        var tr = tbody.children("tr"); //.get();
-    
-        tr.sort(function(a, b) {
-            var A = getVal(a, colIndex);
-            var B = getVal(b, colIndex);
-    
-            if(A < B) {
-                return -1*sortOrder;
-            }
-            if(A > B) {
-                return 1*sortOrder;
-            }
-            return 0;
-        });
-    
-        $.each(tr, function(index, row) {
-            //console.dir(row)
-            tbody.append(row);
-        });
-    
-    });
-    
-});
+        shouldSwitch = false;
+        
+        x = rows[i].getElementsByTagName("td")[n];
+        y = rows[i + 1].getElementsByTagName("td")[n];
+        
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  };
